af1664862f16d3285bd056a7b6cf0f51f5da987e
xgfone <xgfone@126.com>
2023-08-06 15:51:23 +0800

    add the panic handler

--- defaults_time.go
+++ defaults_time.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package defaults provides some global default values.
 package defaults
 
 import (
