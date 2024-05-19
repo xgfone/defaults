b5c3ab5ca8a92b1cf395e3cfc6694d9bca8a8772
xgfone <xgfone@126.com>
2024-05-19 16:38:31 +0800

    add the fatal log function

---
+++ defaults_log.go
@@ -0,0 +1,37 @@
+// Copyright 2024 xgfone
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package defaults
+
+import (
+	"context"
+	"log/slog"
+)
+
+var (
+	// FatalFunc is used to log the message with the FATAL level,
+	// then the program exits.
+	//
+	// Default: use slog.LevelError+4 as the FATAL level, then exit by Exit(1).
+	FatalFunc = NewValueWithValidation(fatal, fA2Validation[string, []any]("Fatal"))
+)
+
+// Fatal is the proxy of FatalFunc to log the message with the FATAL level,
+// then the program exits.
+func Fatal(msg string, args ...any) { FatalFunc.Get()(msg, args) }
+
+func fatal(msg string, args []any) {
+	slog.Log(context.Background(), slog.LevelError+4, msg, args...)
+	Exit(1)
+}
